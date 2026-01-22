5d07e9feb0ac46c964ff4a03481bcd6a09c69f16
Freddy <frederickyamah@gmail.com>
2026-01-21 21:46:25 -0600

    added random color

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".popup");
 const input = document.querySelector(".reset-grid");
+const randomColorButton = document.querySelector(".randomcolor");
+let isRandom = false;
 
 createGrid(10);
 function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const divs = document.createElement("div");
     const squareSize = 480 / size;
-divs.style.width = squareSize + "px";
-divs.style.height = squareSize + "px";
+    divs.style.width = squareSize + "px";
+    divs.style.height = squareSize + "px";
     divs.classList.add("grid-square");
     container.appendChild(divs);
-
-    divs.addEventListener("mouseenter", () => {
-      divs.style.backgroundColor = "blue";
-    });
+      divs.addEventListener("mouseenter", () => {
+        if(isRandom){
+            divs.style.backgroundColor = randomColor()
+        } else {
+        divs.style.backgroundColor = "blue";
+        }
+      });
   }
 }
-function clearGrid(){
-        container.textContent = '';
+function clearGrid() {
+  container.textContent = "";
 }
 
-function resetGrid(){
-    const newSize = input.value;
-    if (isNaN(newSize) || newSize < 1 || newSize > 100){
-        alert("Enter a number between 1 - 100 ");
-        return;
-
-    }
-    clearGrid();
-    createGrid(newSize);
+function resetGrid() {
+  const newSize = input.value;
+  if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+    alert("Enter a number between 1 - 100 ");
+    return;
+  }
+  clearGrid();
+  createGrid(newSize);
 }
 button.addEventListener("click", () => {
-    resetGrid()
+  resetGrid();
 });
+randomColorButton.addEventListener("click", () => {
+  isRandom = true;
+});
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
